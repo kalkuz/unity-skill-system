d35d8e5b603f623995200c0736e5733ff388b95a
Oğuzhan Kalkuz <kalkuzoguzhann@gmail.com>
2023-02-19 18:11:20 +0300

    Implemented Base Skill class, closes #2

---
+++ Runtime/BaseSkill.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace KalkuzSystems.Skill
+{
+  public abstract class BaseSkill : ScriptableObject
+  {
+    [SerializeField] protected string skillName;
+    [SerializeField] protected string description;
+    [SerializeField] protected Sprite icon;
+    
+    public string SkillName => skillName;
+    public string Description => description;
+    public Sprite Icon => icon;
+    
+    public abstract string GetDescription();
+  }
+}
